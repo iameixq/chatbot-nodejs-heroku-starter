af777a182ce21542e023b8a7be98e11dda3db44f
iameixq <iameixq@gmail.com>
2019-02-16 08:47:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,150 +17,167 @@ app.get('/', function (req, res) {
 	res.send('Hello world!!');
 })
 
-app.post('/webhook', middleware(config), (req, res) => {
-  console.log(req.body.events) // webhook event objects
-  console.log(req.body.destination) // user ID of the bot (optional)
-  res.sendStatus(200)
-  Promise.all(req.body.events.map(handleEvent))
+app.use(middleware(config))
+
+app.post('/webhook', (req, res) => {
+    res.sendStatus(200)
+    console.log(req.body.events) // webhook event objects
+    console.log(req.body.destination) // user ID of the bot (optional)
+    Promise
+        .all(req.body.events.map(hanndleEvent))
 })
 
-function handleEvent(event){
-  //if (event.message.type == "text"){
-    /*
-    var parts = event.message.text.split("x");
-    //console.log(event.message.text[0].trim())
-    if (parts[0].trim() == 'สวัสดี') {
+let name;
+let age;
+let question = 0;
 
-        for(var i=1;i<=parts[1].trim();i++){
-          console.log("hi :"+i)
-          let msg = {
-              type: "text",
-              text: "สวัสดี x " + i
+function hanndleEvent(event) {
+    if(question === 1) {
+        question = 2
+        name = event.message.text
+        return client.replyMessage(event.replyToken, {type: 'text', text: `คุณ ${name}อายุเท่าไหร่?`})
+    }else if(question === 2) {
+        question = 0
+        age = event.message.text
+        return client.replyMessage(event.replyToken, {type: 'text', text: `คุณ${name}อายุ ${age} เองครับ...`})
+    }
+    if(event.message.type === 'text' && event.message.text === 'location') {
+        let msg = {
+            "type": "location",
+            "title": "I GEAR GEEK",
+            "address": "malada place",
+            "latitude": 18.7777315,
+            "longitude": 98.9513933
+        }
+        //client.replyMessage(event.replyToken, msg)
+        client.pushMessage(event.source.userId, msg)
+        return client.pushMessage(event.source.userId, msg)
+    }else if(event.message.type === 'text' && event.message.text === 'menu') {
+        let msg = {
+            "type": "template",
+            "altText": "this is a carousel template",
+            "template": {
+                "type": "carousel",
+                "columns": [
+                    {
+                      "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+                      "imageBackgroundColor": "#4286f4",
+                      "title": "this is menu",
+                      "text": "description",
+                      "defaultAction": {
+                          "type": "uri",
+                          "label": "View detail",
+                          "uri": "http://example.com/page/123"
+                      },
+                      "actions": [
+                          {
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=111"
+                          },
+                          {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=111"
+                          },
+                          {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/111"
+                          }
+                      ]
+                    },
+                    {
+                      "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                      "imageBackgroundColor": "#000000",
+                      "title": "this is menu",
+                      "text": "description",
+                      "defaultAction": {
+                          "type": "uri",
+                          "label": "View detail",
+                          "uri": "http://example.com/page/222"
+                      },
+                      "actions": [
+                          {
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=222"
+                          },
+                          {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=222"
+                          },
+                          {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/222"
+                          }
+                      ]
+                    }
+                ],
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover"
             }
-            client.pushMessage(event.source.userId,msg)
-      }
-      return client.pushMessage(event.source.userId,msg)
-      
+          }
+        return client.replyMessage(event.replyToken, msg)
+    }else if(event.message.type === 'text' && event.message.text === 'สวัสดี') {
+        question = 1
+        return client.replyMessage(event.replyToken, { type : "text", text: "สวัสดีคุณชื่ออะไร?"})
+    }else if(event.message.type === 'text' && event.message.text === 'บาย') {
+        question = 0
+        return client.replyMessage(event.replyToken, { type : "text", text: "จบการสนทนา"})
     }
-    */
-  if (event.message.type == "text" && event.message.text == "ดี"){
-    let msg = {
-      type: "text",
-      text: "สวัสดีคุณชื่ออะไร ?"
+    if(event.type === 'message' && event.message.type === 'location') {
+        return handleLocationEvent(event)
     }
-    return client.replyMessage(event.replyToken,msg)
-
-  } else if (event.message.type == "text" && event.message.text == "location"){
-
-    //https://fathomless-reaches-36581.herokuapp.com/api?lat=18.77769165814301&long=98.95345357232975
-
-    var url = 'https://fathomless-reaches-36581.herokuapp.com/api?lat=18.77769165814301&long=98.95345357232975';
-
-    //{ json: true, body: requestData }
-
-    let msg = {
-      // type: "text",
-      // text: event.message.text
-       //id: "325708",
-       type: "location",
-       title: "มิ่งมิตร สาขา Malada Space",
-       address: "ตำบล สุเทพ อำเภอเมืองเชียงใหม่ เชียงใหม่ 50200",
-       latitude: 18.775691 ,
-       longitude: 98.954701
-     }
-
-
-
-    /*
-    let msg = {
-      // type: "text",
-      // text: event.message.text
-       //id: "325708",
-       type: "location",
-       title: "มิ่งมิตร สาขา Malada Space",
-       address: "ตำบล สุเทพ อำเภอเมืองเชียงใหม่ เชียงใหม่ 50200",
-       latitude: 18.775691 ,
-       longitude: 98.954701
-     }
-     // client.replyMessage(event.replyToken,msg)
-     //client.pushMessage(event.source.userId,msg)
-     */
-     //return client.pushMessage(event.source.userId,msg)
-  } else if (event.message.type == "text" && event.message.text == "menu"){
-
-    let msg = {
-      "type": "template",
-      "altText": "this is a carousel template",
-      "template": {
-          "type": "carousel",
-          "columns": [
-              {
-                "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-                "imageBackgroundColor": "#FFFFFF",
-                "title": "this is menu",
-                "text": "description",
-                "defaultAction": {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/123"
-                },
-                "actions": [
-                    {
-                        "type": "postback",
-                        "label": "Buy",
-                        "data": "action=buy&itemid=111"
-                    },
-                    {
-                        "type": "postback",
-                        "label": "Add to cart",
-                        "data": "action=add&itemid=111"
-                    },
-                    {
-                        "type": "uri",
-                        "label": "View detail",
-                        "uri": "http://example.com/page/111"
-                    }
-                ]
-              },
-              {
-                "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-                "imageBackgroundColor": "#000000",
-                "title": "this is menu",
-                "text": "description",
-                "defaultAction": {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/222"
-                },
-                "actions": [
-                    {
-                        "type": "postback",
-                        "label": "Buy",
-                        "data": "action=buy&itemid=222"
-                    },
-                    {
-                        "type": "postback",
-                        "label": "Add to cart",
-                        "data": "action=add&itemid=222"
-                    },
-                    {
-                        "type": "uri",
-                        "label": "View detail",
-                        "uri": "http://example.com/page/222"
-                    }
-                ]
-              }
-          ],
-          "imageAspectRatio": "rectangle",
-          "imageSize": "cover"
-      }
-    }
-    return client.replyMessage(event.replyToken,msg)
-  }
 }
 
+
+function handleLocationEvent(event) {
+    apiUrl = 'https://fathomless-reaches-36581.herokuapp.com/api'
+    return new Promise((resolve, reject) => {
+    restClient.get(`${apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
+        if (data) {
+          const pinData = data.map(row => ({
+            "thumbnailImageUrl": row.aqi.icon,
+            "imageBackgroundColor": "#FFFFFF",
+            "title": `PM 2.5: ${row.aqi.aqi}`,
+            "text": `${row.nameTH}, ${row.areaTH}`,
+            "actions": [
+              {
+                "type": "uri",
+                "label": "ข้อมูลย้อนหลัง",
+                "uri": row.historyUrl
+              }
+            ]
+          }))
+      
+          var msg = {
+            "type": "template",
+            "altText": "ข้อมูลสถานที่",
+            "template": {
+              "type": "carousel",
+              "columns": pinData,
+              "imageAspectRatio": "rectangle",
+              "imageSize": "cover"
+            }
+          }
+  
+          resolve(client.replyMessage(event.replyToken, msg))
+        } else {
+          reject()
+        }
+      })
+    })
+   
+  }
+
 app.set('port', (process.env.PORT || 4000))
+
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
 })
+
+
 
